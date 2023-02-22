6369bdea60dbea11c8819cf0fd607a12fd7b53b6
GMontanher96 <gabrielmontanherr@gmail.com>
2023-02-22 20:46:19 -0300

    add

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes as Switch, Navigate } from "react-router-dom"
+
+export const Routes = () => {
+    return (
+        <BrowserRouter>
+        <Switch>
+            <Route path="/login" />
+            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+        </Switch>
+        </BrowserRouter>
+    )
+}
